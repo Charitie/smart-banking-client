be10b8faf38975a79411485f6ec359b2a47e2483
charitie <charitytarus21@gmail.com>
2020-10-23 05:04:36 +0300

    build file and firabase deployment files

---
+++ build/precache-manifest.583816b5cac60fa1636997c19a759ed1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ab057b5de88350e0140fc7c27865af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be241d8120246ad96095",
+    "url": "/static/css/main.33c5bc71.chunk.css"
+  },
+  {
+    "revision": "c4779e78f32a46402693",
+    "url": "/static/js/2.a1e34fc1.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/static/js/2.a1e34fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be241d8120246ad96095",
+    "url": "/static/js/main.1f8799bb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "465995f810b5142f78ed66c4489c5525",
+    "url": "/static/media/bg.465995f8.jpeg"
+  },
+  {
+    "revision": "169c34f7ac47274e5f36954a54828b0a",
+    "url": "/static/media/bg2.169c34f7.jpeg"
+  },
+  {
+    "revision": "2345e2c3e18cf7a1856fd99237a9f643",
+    "url": "/static/media/bg3.2345e2c3.jpeg"
+  },
+  {
+    "revision": "2446d46ce9180fab1c14e82180aec871",
+    "url": "/static/media/cancel-circle.2446d46c.svg"
+  },
+  {
+    "revision": "90c951b6bf1f4ebf6c875a9bda23207b",
+    "url": "/static/media/card.90c951b6.png"
+  },
+  {
+    "revision": "ee04e32ee533460f1262e9c513c44b8b",
+    "url": "/static/media/menu.ee04e32e.svg"
+  },
+  {
+    "revision": "8de22a8e109ef280d8d5fd0a7d5f9c66",
+    "url": "/static/media/profile.8de22a8e.jpeg"
+  }
+]);
