35740fa640120000867fa67fabc665d4b38dcc88
charitie <charitytarus21@gmail.com>
2020-10-23 04:45:56 +0300

    modal reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import auth from "./authReducers";
 import alert from './alertReducer';
 import account from './accountReducer';
+import modal from './modalReducer';
 
 export default combineReducers({
   auth,
   account,
-  alert
+  alert,
+  modal
 });
